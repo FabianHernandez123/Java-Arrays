bc8615b980027268975dc90a1ba19a5256dfb73d
Fabian Hernandez <h.fabian25f@gmail.com>
2021-02-02 14:30:00 -0600

    Evaluating the index value

--- src/dataStructures2/Main.java
+++ src/dataStructures2/Main.java
@@ -10,21 +10,21 @@ public class Main {
 		}
 		System.out.printf("the sum is %d\n", addedUp);
 	}
+	
 	public static int[] toPower(int size, int power) {
 		int powerArray[] = new int[size];
 			for (int j = 0; j< size; j++) {
 				powerArray[j]=(int) Math.pow(j, power);	
 			}
-		return powerArray;
-		
+		return powerArray;	
 	}
 
 	public static void main(String[] args) {
-		//calling sumArray method
+		//1. calling sumArray method
 		Integer[] example1 = {5,1};
 		sumArray(example1);
 		
-		//trying given code
+		//2. trying given code
 		double[ ] exampleArray = {1,5,6,5,4,1};
         double maximum = exampleArray[0];
 
@@ -34,13 +34,15 @@ public class Main {
              if (exampleArray[ i ] > maximum) {
                   maximum = exampleArray[ i ];
                   index = i;
-                  System.out.printf("%d, %d\n",(int)exampleArray[i],(int)index);
+                  //System.out.printf("%d, %d\n",(int)exampleArray[i],(int)index);
+                  //index stopped increasing after index two, where the element
+                  //with a value of 6 was present. 
              }
         }
-        //System.out.println(index);
+        System.out.println(index);
 		
         
-        //printing the toPower method to see if it works. 
+        //3. printing the toPower method to see if it works. 
         System.out.println(Arrays.toString(toPower(4, 2)));
 		
 		
