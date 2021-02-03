f98cb813f4bcfc62f5d522362f825d1785774cd5
Fabian Hernandez <h.fabian25f@gmail.com>
2021-02-03 15:34:01 -0600

    Fixed some spacing and changed print to return in sumArray method

--- src/dataStructures2/Main.java
+++ src/dataStructures2/Main.java
@@ -3,12 +3,12 @@ package dataStructures2;
 import java.util.Arrays;
 
 public class Main {
-	static void sumArray(Integer[] examples) {
+	static int sumArray(Integer[] examples) {
 		Integer addedUp = 0;
 		for(Integer example: examples) {
 			addedUp += example;
 		}
-		System.out.printf("the sum is %d\n", addedUp);
+		return addedUp;
 	}
 	
 	public static int[] toPower(int size, int power) {
@@ -22,17 +22,15 @@ public class Main {
 	public static void main(String[] args) {
 		//1. calling sumArray method
 		Integer[] example1 = {5,1};
-		sumArray(example1);
+		System.out.println(Integer.toString(sumArray(example1)));
 		
 		//2. trying given code
-		double[ ] exampleArray = {1,5,6,5,4,1};
+		double[] exampleArray = {1,5,6,5,4,1};
         double maximum = exampleArray[0];
-
         int index = 0;
         for (int i = 1; i<exampleArray.length; i++){
-
-             if (exampleArray[ i ] > maximum) {
-                  maximum = exampleArray[ i ];
+             if (exampleArray[i] > maximum) {
+                  maximum = exampleArray[i];
                   index = i;
                   //System.out.printf("%d, %d\n",(int)exampleArray[i],(int)index);
                   //index stopped increasing after index two, where the element
